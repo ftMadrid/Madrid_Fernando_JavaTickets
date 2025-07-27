0dd3ca8601b285c9531c8ae50baca2d80ff38f51
ftMadrid <fernandomadridcv04@gmail.com>
2025-07-26 18:57:29 -0600

    First Commit

---
+++ Madrid_Fernando_JavaTickets/src/javatickets/JavaTickets.java
@@ -0,0 +1,11 @@
+package javatickets;
+
+public class JavaTickets {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        
+    }
+    
+}
