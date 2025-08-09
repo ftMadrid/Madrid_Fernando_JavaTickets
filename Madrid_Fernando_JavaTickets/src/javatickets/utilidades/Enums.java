1de6481024e71b7bf9510dc850a0d09ac5658626
ftMadrid <fernandomadridcv04@gmail.com>
2025-08-09 15:41:53 -0600

    Agregadas algunas cosas en Enums

--- Madrid_Fernando_JavaTickets/src/javatickets/utilidades/Enums.java
+++ Madrid_Fernando_JavaTickets/src/javatickets/utilidades/Enums.java
@@ -21,4 +21,61 @@ public class Enums {
         
     }
     
+    public enum TipoEventos {
+        
+        DEPORTIVO("Deportivo"),
+        MUSICAL("Musical"),
+        RELIGIOSO("Religioso");
+        
+        private final String nombre;
+        
+        TipoEventos(String nombre){
+            this.nombre = nombre;
+        }
+        
+        @Override
+        public String toString(){
+            return nombre;
+        }
+    }
+    
+    public enum TipoDeportes {
+        
+        FUTBOL("Futbol"),
+        TENIS("Tenis"),
+        RUGBY("Rugby"),
+        BASEBALL("Baseball");
+        
+        private final String nombre;
+        
+        TipoDeportes(String nombre) {
+            this.nombre = nombre;
+        }
+        
+        @Override
+        public String toString(){
+            return nombre;
+        }
+    }
+    
+    public enum TipoMusica {
+        
+        POP("POP"),
+        ROCK("Rock"),
+        RAP("RAP"),
+        CLASICA("Clasica"),
+        REGGEATON("Reggeaton"),
+        OTRP("Otro");
+        
+        private final String nombre;
+        
+        TipoMusica(String nombre) {
+            this.nombre = nombre;
+        }
+        
+        @Override
+        public String toString(){
+            return nombre;
+        }
+    }
 }
